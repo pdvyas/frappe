b16226442e4c3e7791972c7b6db05a906ac491b0
Rushabh Mehta <rmehta@gmail.com>
2012-07-25 14:11:52 +0530

    removed webnotes.model.docfield

--- py/webnotes/model/utils.py
+++ py/webnotes/model/utils.py
@@ -107,50 +107,6 @@ def getlist(doclist, field):
 
 	return l
 
-def getvaluelist(doclist, fieldname):
-	"""
-		Returns a list of values of a particualr fieldname from all Document object in a doclist
-	"""
-	l = []
-	for d in doclist:
-		l.append(d.fields[fieldname])
-	return l
-
-def _make_html(doc, link_list):
-
-	from webnotes.utils import cstr
-	out = '<table class="simpletable">'
-	for k in doc.fields.keys():
-		if k!='server_code_compiled':
-			v = cstr(doc.fields[k])
-
-			# link field
-			if v and (k in link_list.keys()):
-				dt = link_list[k]
-				if isinstance(dt, basestring) and dt.startswith('link:'):
-					dt = dt[5:]
-				v = '<a href="index.cgi?page=Form/%s/%s">%s</a>' % (dt, v, v)
-
-			out += '\t<tr><td>%s</td><td>%s</td></tr>\n' % (cstr(k), v)
-
-	out += '</table>'
-	return out
-
-def to_html(doclist):
-	"""
-	Return a simple HTML format of the doclist
-	"""
-	out = ''
-	link_lists = {}
-
-	for d in doclist:
-		if not link_lists.get(d.doctype):
-			link_lists[d.doctype] = d.make_link_list()
-
-		out += _make_html(d, link_lists[d.doctype])
-
-	return out
-
 def commonify_doclist(doclist, with_comments=1):
 	"""
 		Makes a doclist more readable by extracting common properties.
