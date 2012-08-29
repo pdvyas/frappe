5fc0ec26c8aabe26e8ed18264bce915fea15d4c1
Rushabh Mehta <rmehta@gmail.com>
2012-08-29 12:14:52 +0530

    form update

--- py/webnotes/model/controller.py
+++ py/webnotes/model/controller.py
@@ -279,7 +279,7 @@ class DocListController(object):
 	def export(self):
 		"""export current doc to file system"""
 		import conf
-		if not getattr(webnotes, 'syncing', False) and 
+		if not getattr(webnotes, 'syncing', False) and \
 			getattr(conf,'developer_mode', 0) and not getattr(webnotes, "test_mode", 0):
 			from webnotes.modules.export import export_to_files
 			export_to_files(record_list=self.doclist)
