b6d362353cca97e3ebb3dbfb1761c724334f8025
Pratik Vyas <m@pd.io>
2015-07-02 10:27:29 +0530

    websocket poc

--- frappe/app.py
+++ frappe/app.py
@@ -12,6 +12,8 @@ from werkzeug.local import LocalManager
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.contrib.profiler import ProfilerMiddleware
 from werkzeug.wsgi import SharedDataMiddleware
+from socketio.server import SocketIOServer
+
 
 import mimetypes
 import frappe
@@ -22,6 +24,7 @@ import frappe.utils.response
 import frappe.website.render
 from frappe.utils import get_site_name
 from frappe.middlewares import StaticDataMiddleware
+from websocket import SocketIO, emit, send, join_room
 
 local_manager = LocalManager([frappe.local])
 
@@ -29,6 +32,21 @@ _site = None
 _sites_path = os.environ.get("SITES_PATH", ".")
 
 logger = frappe.get_logger()
+
+class RequestContext(object):
+
+	def __init__(self, environ):
+		self.request = Request(environ)
+
+	def __enter__(self):
+		frappe.local.request = self.request
+		init_site(self.request)
+		make_form_dict(self.request)
+		frappe.local.http_request = frappe.auth.HTTPRequest()
+
+	def __exit__(self, type, value, traceback):
+		frappe.destroy()
+		
 
 @Request.application
 def application(request):
@@ -134,6 +152,17 @@ def make_form_dict(request):
 		frappe.local.form_dict.pop("_")
 
 application = local_manager.make_middleware(application)
+application.debug = True
+socketio = SocketIO(application)
+
+@socketio.on('task', namespace='')
+def subscribe_to_task(message):
+	if message.get('task_id'):
+		join_room('task:' + message.get('task_id'))
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
 
 def serve(port=8000, profile=False, site=None, sites_path='.'):
 	global application, _site, _sites_path
@@ -141,6 +170,7 @@ def serve(port=8000, profile=False, site=None, sites_path='.'):
 	_sites_path = sites_path
 
 	from werkzeug.serving import run_simple
+	application = socketio.app
 
 	if profile:
 		application = ProfilerMiddleware(application, sort_by=('tottime', 'calls'))
@@ -154,5 +184,31 @@ def serve(port=8000, profile=False, site=None, sites_path='.'):
 			b'/files': os.path.abspath(sites_path).encode("utf-8")
 		})
 
-	run_simple('0.0.0.0', int(port), application, use_reloader=True,
-		use_debugger=True, use_evalex=True, threaded=True)
+	application.debug = True
+	application.config = {
+		'SERVER_NAME': 'localhost:8000'
+	}
+	from gevent import monkey, Greenlet
+	monkey.patch_all()
+	Greenlet.spawn(listen_on_redis)
+	SocketIOServer(('0.0.0.0', 8080), application,
+				   resource="socket.io", policy_server=True,
+				   policy_listener=('0.0.0.0', 10843)).serve_forever()
+
+	# run_simple('0.0.0.0', int(port), application, use_reloader=True,
+	# 	use_debugger=True, use_evalex=True, threaded=True)
+
+
+def listen_on_redis():
+	import redis
+	from time import sleep
+	r = redis.Redis(port=11311)
+	pubsub = r.pubsub()
+	pubsub.subscribe('events')
+	for message in pubsub.listen():
+		try:
+			msg = json.loads(message['data'])
+			if msg.get('event'):
+				socketio.emit(msg['event'], msg['message'], room=msg['room'])
+		except Exception, e:
+			print e.message
